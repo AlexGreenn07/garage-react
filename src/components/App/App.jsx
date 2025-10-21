e9234868e7309ee04e4646ccde5f3cf367310161
Alex <t9169905831@gmail.com>
2025-10-21 22:28:52 +0300

    First app commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { MyName } from "../MyName/MyName.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Привет, React!</h1>
+        <p> test React</p>
+        <MyName />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,7 @@
+export function MyName() {
+  return (
+    <h2>
+      Меня зовут Саша <br /> Привет
+    </h2>
+  );
+}
