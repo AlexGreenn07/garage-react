401dd59f23814d7a4d3d7a3c3c1eeedf14dc934b
Alex <t9169905831@gmail.com>
2025-10-22 21:34:09 +0300

    1.6 Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,12 @@
 import { MyName } from "../MyName/MyName.jsx";
+import Header from "../Header/Header.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Привет, React!</h1>
-        <p> test React</p>
-        <MyName />
-      </div>
-    </>
+    <main className="app">
+      <Header></Header>
+    </main>
   );
 }
 
