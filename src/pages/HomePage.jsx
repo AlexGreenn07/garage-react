3ace51b0a901671dff8683998c1f23eae5ae2401
Alex <t9169905831@gmail.com>
2025-10-27 20:29:21 +0300

    change submit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,30 @@
-import { UserList } from "../components/UserList";
+import { useState } from "react";
 import "./HomePage.css";
 
-function HomePage({ click, setClick }) {
-  const handleClick = () => {
-    setClick(click + 1);
-  };
+function HomePage() {
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Форма отправлена");
+  }
 
   return (
-    <div className="home-page">
-      <h2>Домашняя страница</h2>
-      <p>Добро пожаловать на наш сайт!</p>
-      <button onClick={handleClick}>Нажми меня</button>
-      <p>{click}</p>
-      <UserList></UserList>
-    </div>
+    <>
+      <div className="home-page">
+        <h2>Домашняя страница</h2>
+        <p>Добро пожаловать на наш сайт!</p>
+        <button>Нажми меня</button>
+        <input type="text" onChange={handleChange} />
+        <p>{value}</p>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Отправить</button>
+        </form>
+      </div>
+    </>
   );
 }
 export default HomePage;
